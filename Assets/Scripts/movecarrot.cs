fc88422f3c416eb692af695f6ab9e10ff6a22988
DESKTOP-HQEQLHD\HyeRi <kbi09@naver.com>
2017-10-25 14:32:16 +0900

    먹이 당근 아이템 추가함

---
+++ Assets/Scripts/movecarrot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movecarrot : MonoBehaviour
+{
+    public float carrotSpeed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * carrotSpeed * Time.deltaTime);
+
+        if (this.transform.position.x < -10f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+  
+}

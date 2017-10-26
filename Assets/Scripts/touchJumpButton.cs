1a3373ef7cf82c75683c7eb56188ffafe3c7ab0d
DESKTOP-KAKUKTV\KIMGAYEON <yellow139@naver.com>
2017-10-26 13:21:04 +0900

    달리는 토끼의 jump script를 올림

---
+++ Assets/Scripts/touchJumpButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchJumpButton : MonoBehaviour {
+
+    private GameObject target;
+    public GameObject jumpbutton;
+    public float jump;
+
+    public void Update()
+    {
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            CastRay();
+            if (target == jumpbutton)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+            }
+        }
+        
+    }
+
+    void CastRay()
+    {
+        target = null;
+
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+
+        
+        if(hit.collider != null)
+        {
+            target = hit.collider.gameObject;
+        }
+        
+    }
+}

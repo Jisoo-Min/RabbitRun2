b74900097344c32da6ceb9e21f0b2626b410f7eb
DESKTOP-KAKUKTV\KIMGAYEON <yellow139@naver.com>
2017-10-29 23:39:39 +0900

    이단 점프기능을 구현함. 계속 이어지도록 수정해야함.

--- Assets/Scripts/touchJumpButton.cs
+++ Assets/Scripts/touchJumpButton.cs
@@ -5,24 +5,68 @@ using UnityEngine;
 
 public class touchJumpButton : MonoBehaviour
 {
-
+    private float jump = 8;
+    private int jump_number;
+    private bool is_onground;
     private GameObject target; //클릭한 곳
     public GameObject jumpbutton; //점프버튼
-    private float jump = 8; // jump 하는 값
-    private bool isJump = false;
+
+    void OnCollisionEnter() //접촉했을때
+    {
+        jump_number = 0;
+        is_onground = true;
+    }
+
+    void OnCollisionExit() //접촉하지 않았을때
+    {
+        is_onground = false;
+    }
+
+    public int jump_track() //점프 횟수 세기
+    {
+        if (is_onground == true)
+        {
+            jump_number = 0;
+        }
+
+        if (target == jumpbutton)
+        {
+            ++jump_number;
+            Debug.Log(jump_number);
+        }
+        return jump_number;
+    }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+
+        if (Input.GetMouseButtonDown(0)) // 화면이 클릭되었을때
         {
             CastRay();
-            isJump = true;
-            if ((target == jumpbutton) && isJump)
+            jump_track();
+            if (is_onground == true)
             {
-                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
-                isJump = false;
+                if (target == jumpbutton)
+                {
+                    rigidbody.velocity = new Vector2(rigidbody.velocity.x, jump);
+                }
+            }
+
+            else if (is_onground == false)
+            {
+                if ((target == jumpbutton) && jump_number <= 2)
+                {
+                    rigidbody.velocity = new Vector2(rigidbody.velocity.x, jump);
+                }
+
+                if (jump_number > 2) //점프횟수가 2번이 넘었을때
+                {
+                    rigidbody.AddForce(new Vector2(0, 0));
+                }
             }
         }
+
 
     }
 
@@ -33,72 +77,10 @@ public class touchJumpButton : MonoBehaviour
         Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
 
-        if (hit.collider != null)
+        if (hit.collider != null) // 클릭되었다면 실행
         {
-            target = hit.collider.gameObject;
+            target = hit.collider.gameObject; //클릭된 게임 오브젝트를 타겟으로 지정
         }
 
     }
 }
-
-/* button UI 이용해서 하는거 - 수정 필요
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
-
-public class touchJumpButton : MonoBehaviour
-{
-    private bool jumpOn = false;
-    private int checkTime = 0;
-    private int jump = 7;
-
-    void Update()
-    {
-        if (!jumpOn) 
-        {
-            if ((Input.GetMouseButtonDown(0))) // 버튼을 누름
-            {
-                if(EventSystem.current.IsPointerOverGameObject() == false)
-                { // UI 위가 아니면
-                    StartCoroutine("CheckButtonDownSec");
-                }
-            }
-            else if((Input.GetMouseButtonUp(0))) // 버튼에서 땜.
-            {
-                if(EventSystem.current.IsPointerOverGameObject() == false)
-                { //UI 위가 아니면
-                    if (checkTime > 0)
-                    { // 점프 수치가 있어야 실행함.
-                        StartCoroutine("JumpAction");
-                        CastRay();
-                        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
-                    }
-                }
-                else
-                { // UI위라면 점프실행X & 시간 측정 끔.
-                    if(checkTime>0)
-                    {
-                        StopCoroutine("CheckButtonDownSec");
-                        checkTime = 0;
-                    }
-                }
-            }
-        }
-    }
-
-    void CastRay()
-    {
-        target = null;
-
-        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
-
-        if (hit.collider != null)
-        {
-            target = hit.collider.gameObject;
-        }
-
-    }
-}
-*/

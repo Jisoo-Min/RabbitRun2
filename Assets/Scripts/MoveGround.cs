b9963d8cb150cb1340baf1081b63fb2b1d789bb2
DESKTOP-QM02H8I\jisoo <jsmin0415@naver.com>
2017-10-31 17:26:20 +0900

    그라운드 프리팹 생성

---
+++ Assets/Scripts/MoveGround.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGround : MonoBehaviour {
+
+    public float groundSpeed;
+    public float outOfRangeX;
+    //float timeSpeed = 0f;
+	// Use this for initialization
+	void Start () {
+ 
+	}
+
+    void Update() {
+
+        transform.Translate(Vector3.left * groundSpeed * Time.deltaTime);
+
+        if (this.transform.position.x < outOfRangeX)
+        {
+             Destroy(this.gameObject);
+            
+        }
+    }
+
+}

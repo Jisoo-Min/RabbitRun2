26932135ecc5bc7fe026cb390e5dd2c56753d468
DESKTOP-QM02H8I\jisoo <jsmin0415@naver.com>
2017-11-02 13:08:37 +0900

    pauseGame  주석 추가

--- Assets/Scripts/pauseGame.cs
+++ Assets/Scripts/pauseGame.cs
@@ -16,7 +16,7 @@ public class PauseGame : MonoBehaviour
     void Start()
     {
 
-        pausePanel.SetActive(false);
+        pausePanel.SetActive(false); //해당 Panel을 활성화
         resultPanel.SetActive(false);
 
     }
@@ -29,14 +29,14 @@ public class PauseGame : MonoBehaviour
 
     public void Pause()
     {
-        Time.timeScale = 0;
+        Time.timeScale = 0; //화면정지
         pauseButton.SetActive(false);
         pausePanel.SetActive(true);
     }
 
     public void Unpause()
     {
-        Time.timeScale = 1;
+        Time.timeScale = 1; //화면 정지 해제
         pauseButton.SetActive(true);
         pausePanel.SetActive(false);
     }

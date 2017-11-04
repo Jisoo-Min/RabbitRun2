21106781c39605c9eb6416f9727629a6cea1e68d
DESKTOP-QM02H8I\jisoo <jsmin0415@naver.com>
2017-11-04 17:05:54 +0900

    add rabbit to tag "rabbit" ,  edit "BumpIntoObstacle.cs"

--- Assets/Scripts/BumpIntoObstacle.cs
+++ Assets/Scripts/BumpIntoObstacle.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BumpIntoObstacle : MonoBehaviour {
 
 	// Use this for initialization
@@ -13,4 +14,12 @@ public class BumpIntoObstacle : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    void OnCollisionEnter2D(Collision2D item)
+    {
+       if(item.gameObject.name == "rabbit")
+        {
+            iTween.ShakePosition(Camera.main.gameObject, iTween.Hash("x", 0.2, "y", 0.2, "time", 0.5f));
+        }
+    }
 }

16a623d4321996b886f8d35182bcc321750444f7
DESKTOP-QM02H8I\jisoo <jsmin0415@naver.com>
2017-11-21 17:16:24 +0900

    flag에 충돌하면 장애물크기 바뀌게 수정중

--- Assets/Scripts/MoveObstacle.cs
+++ Assets/Scripts/MoveObstacle.cs
@@ -2,26 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveObstacle : MonoBehaviour {
-   
+public class MoveObstacle : MonoBehaviour
+{
+
     public static float obstacleSpeed = 5;
     public float outOfRangeX;
     //float timeSpeed = 0f;
-	// Use this for initialization
-	void Start () {
- 
-	}
+    // Use this for initialization
+    void Start()
+    {
+        this.transform.localScale = new Vector2(this.transform.localScale.x * 0.5f, this.transform.localScale.y * 0.5f);
 
+    }
     // Update is called once per frame
-    void Update() {
-        
+    void Update()
+    {
+
         transform.Translate(Vector2.left * obstacleSpeed * Time.deltaTime);
 
         if (this.transform.position.x < outOfRangeX)
         {
-             Destroy(this.gameObject);
-            
+            Destroy(this.gameObject);
+
         }
     }
-    
+    void OnTriggerEnter2D(Collider2D item)   
+    {
+      // if(item.tag=="flag")
+            this.transform.localScale = new Vector2(2f, 2f);
+        
+    }
 }

20fc3283c875fcd02c6da5b678ee817f35c48c91
DESKTOP-QM02H8I\jisoo <jsmin0415@naver.com>
2017-11-28 20:22:44 +0900

    자석효과 추가 ^0^, 버튼 클릭시 이미지 바뀌게 수정

---
+++ Assets/Scripts/Magnetic.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnetic : MonoBehaviour {
+    public float acc;
+    public float range;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    private void FixedUpdate() //update된 후 수행
+    {
+        UpdateMag("carrot");
+        UpdateMag("coin");
+    }
+
+    void UpdateMag(string tag) 
+    {
+        var tags = GameObject.FindGameObjectsWithTag(tag); //해당 태그를 배열로 생성
+        foreach (var item in tags)
+        {
+            Vector3 dir = transform.position - item.transform.position; //캐릭터위치-item위치(벡터값)
+            if (dir.magnitude > range) //범위안에 들어오면 magnitude = 길이
+                continue;
+
+            dir.Normalize(); //단위벡터 
+
+            item.transform.position = item.transform.position + dir * acc * Time.deltaTime;//가속도 붙어서 날라옴...
+        }
+    }
+}

8c0ca19b9064d99fa0f8640df88040f23eab4fb6
DESKTOP-QM02H8I\jisoo <jsmin0415@naver.com>
2017-11-29 14:33:08 +0900

    지수 캐릭터에서 스푼추가

---
+++ Assets/Scripts/Strong.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class Strong : MonoBehaviour {
+
+    public GameObject spoon;
+
+    // Use this for initialization
+    void Start () {
+        spoon.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (CharacterName.characterName.Equals("JiSoo"))
+        {
+            spoon.SetActive(true);
+        }
+        
+
+
+    }
+    void UpdateStrong(string tag)
+    {
+     //   transform.Rotate(Vector3(1, 0, 0) * speed * Time.deltaTime);
+    }
+    
+}
